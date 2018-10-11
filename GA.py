e4482b4a2d979996dff2bae8f16ed074261bb990
Xun <xun468@yahoo.com>
2018-10-12 00:46:43 +0200

    disabled no move option for now, some readability fixes

--- GA.py
+++ GA.py
@@ -10,14 +10,15 @@ from deap import algorithms
 
 import create_maze_graph as cm
 
-MAX_MOVES = 10
+MAX_MOVES = 20
 maze_easy = cm.create_maze(4,4,0.75,0.75)
+maze_test = cm.create_maze(5,5,0.8,0.8)
+maze = maze_test
 
-maze = maze_easy
-
+print(maze)
 #TODO:: THIS IS COMPLETELY ARBITRARY!!
 start = [0,0]
-end = [0,4]
+end = [2,2]
 #Heuristic/fitness function. Input is individual (can treat like a list)
 #output is score, Note: the comma is important
 def evalOneMax(individual):
@@ -25,8 +26,11 @@ def evalOneMax(individual):
     return sum(individual),
 
 def evalMaze(ind, verbose = False):
+    seen = set()
     penalty = 0
-    c_c = start
+    c_c=[0,0]
+    c_c[0] = start[0]
+    c_c[1] = start[1]
     n_c = [-1,-1]
     move_counter = 1
     early_stop = 0
@@ -34,40 +38,41 @@ def evalMaze(ind, verbose = False):
     for i in range(len(ind)):
         n_c[0] = c_c[0]
         n_c[1] = c_c[1]
+        seen.add((c_c[0],c_c[1]))
         if ind[i] == 1:
-            if verbose:
-                print("attempting to move up")
             n_c[0] = c_c[0] - 1
+            if verbose:
+                print("moving up " + str(c_c) + " to " + str(n_c))
         elif (ind[i] == 2):
-            if verbose:
-                print("attempting to move right")
             n_c[1] = c_c[1] + 1
+            if verbose:
+                print("moving right " + str(c_c) + " to " + str(n_c))
         elif (ind[i] == 3):
-            if verbose:
-                print("attempting to move down")
             n_c[0] = c_c[0] + 1
-        elif (ind[i] == 4):
             if verbose:
-                print("attempting to move left")
+                print("moving down " + str(c_c) + " to " + str(n_c))
+        elif (ind[i] == 4):
             n_c[1] = c_c[1] - 1
+            if verbose:
+                print("moving left " + str(c_c) + " to " + str(n_c))
         nx = n_c[0]
         ny = n_c[1]
 
         if (nx >= len(maze) or ny >= len(maze[0]) or nx < 0 or ny < 0):
             if verbose:
                 print("OUT OF BOUNDS")
-                print(c_c)
-                print(n_c)
             penalty+=1
 
         elif(maze[nx][ny] == False):
             if verbose:
                 print("INVALID MOVE")
-                print(c_c)
-                print(n_c)
             penalty+=1
         else:
-            c_c = n_c
+            c_c[0] = n_c[0]
+            c_c[1] = n_c[1]
+            if((c_c[0],c_c[1]) in seen):
+                if verbose: print("seen")
+                penalty+=2
             if(c_c == end):
                 early_stop = i+1
                 if verbose:
@@ -92,7 +97,7 @@ creator.create("Individual", list, fitness=creator.FitnessMax)
 toolbox = base.Toolbox()
 
 #generation function for an element of the individual
-toolbox.register("attr_bool", random.randint, 0, 4)
+toolbox.register("attr_bool", random.randint, 1, 4)
 
 #structure initializers, int at the end is size of the individual
 toolbox.register("individual", tools.initRepeat, creator.Individual,
@@ -108,13 +113,13 @@ toolbox.register("evaluate", evalMaze)
 toolbox.register("mutate", tools.mutUniformInt, indpb=0.05,low = 0, up = 4)
 
 #idk
-toolbox.register("mate", tools.cxTwoPoint)
+toolbox.register("mate", tools.cxOnePoint)
 toolbox.register("select", tools.selTournament, tournsize=3)
 
-pop = toolbox.population(n = 500)
+pop = toolbox.population(n = 2000)
 hof = tools.HallOfFame(1)
 
-pop, log = algorithms.eaSimple(pop, toolbox, cxpb=0.5, mutpb=0.2, ngen=40,
+pop, log = algorithms.eaSimple(pop, toolbox, cxpb=0.50, mutpb=0.25, ngen=60,
                                halloffame=hof, verbose=True)
 
 print(hof[0])
