5fb86f09ad42fd32453c77f2ebdc9170da128f56
Xun <xun468@yahoo.com>
2018-10-16 13:35:18 +0200

    added new algorithm

--- GA.py
+++ GA.py
@@ -10,34 +10,50 @@ from deap import algorithms
 
 import create_maze_graph as cm
 
-def mazePrint(maze,goal,start):
+def mazePrint(maze,goal,start,start2):
+    wall = "■ ■ "
+    for i in range(len(maze)):
+        wall+= "■ "
+    print(wall)
     for i in range(len(maze)):
         line = "■ "
         for j in range(len(maze[0])):
             if([i,j] == goal):
-                line = line + "X "
+                line = line + "G "
             elif([i,j] == start):
                 line = line + "O "
+            elif([i,j] == start2):
+                line = line + "X "
             elif (maze[i][j] == True):
                 line = line + "  "
             else:
                 line = line + "■ "
         line = line + "■ "
         print(line)
+    print(wall)
 
 MAX_MOVES = 20
 maze_test = cm.create_maze(4,4,0.75,0.75)
 maze_easy = cm.create_maze(5,5,0.8,0.8)
 maze_medium = cm.create_maze(8,8,0.25,0.25)
 maze_hard = cm.create_maze(10,10,0.40,0.2)
-maze_extreme = cm.create_maze(30,30,0.6,0.75)
-maze = maze_medium
 
-#TODO:: THIS IS COMPLETELY ARBITRARY!!
+maze = maze_test
+
+#Test
+#[0,0],[0,1],[4,4]
+#Easy
+#[0,0],[4,0],[2,2]
+#Medium
+#[0,0],[0,8],[4,4]
+#hard
+#[0,0],[0,10],[2,8]
+
 start = [0,0]
+start2 = [0,4]
 end = [4,4]
 
-mazePrint(maze,end,start)
+mazePrint(maze,end,start,start2)
 #Heuristic/fitness function. Input is individual (can treat like a list)
 #output is score, Note: the comma is important
 def evalOneMax(individual):
@@ -51,12 +67,12 @@ def evalMaze(ind, verbose = False):
     c_c[0] = start[0]
     c_c[1] = start[1]
     n_c = [-1,-1]
-    move_counter = 1
-    early_stop = 0
+    path_length = 0
 
     for i in range(len(ind)):
         n_c[0] = c_c[0]
         n_c[1] = c_c[1]
+        path_length += 1
         seen.add((c_c[0],c_c[1]))
         if ind[i] == 1:
             n_c[0] = c_c[0] - 1
@@ -96,20 +112,21 @@ def evalMaze(ind, verbose = False):
                 if verbose: print("seen")
                 penalty+=1
             if(c_c == end):
-                early_stop = i+1
                 if verbose:
                     print("Reached Goal!!")
                 break
 
     #calculate distance from goal
     distance = math.sqrt((end[0]-c_c[0])**2 + (end[1]-c_c[1])**2)
-    path_length = (MAX_MOVES-early_stop)
+    bonus = 0
+    if distance == 0:
+        bonus = MAX_MOVES-path_length + 100
     if verbose:
         print("distance to goal:" + str(distance))
         print("penalty:" + str(penalty))
         print("path length:"+ str(path_length))
 
-    return distance + penalty,
+    return distance + penalty - bonus,
 #determins if we are minimizing or maximizing
 creator.create("FitnessMax", base.Fitness, weights=(-1.0,))
 
@@ -138,12 +155,12 @@ toolbox.register("mutate", tools.mutUniformInt, indpb=0.05,low = 1, up = 4)
 toolbox.register("mate", tools.cxOnePoint)
 toolbox.register("select", tools.selTournament, tournsize=3)
 
-pop = toolbox.population(n = 1000)
-hof = tools.HallOfFame(1)
-
-pop, log = algorithms.eaSimple(pop, toolbox, cxpb=0.50, mutpb=0.25, ngen=70,
-                               halloffame=hof, verbose=True)
-
-print(hof[0])
-
-print(evalMaze(hof[0],verbose = True))
+# pop = toolbox.population(n = 1000)
+# hof = tools.HallOfFame(1)
+#
+# pop, log = algorithms.eaSimple(pop, toolbox, cxpb=0.50, mutpb=0.25, ngen=70,
+#                                halloffame=hof, verbose=True)
+#
+# print(hof[0])
+#
+# print(evalMaze(hof[0],verbose = True))
--- MultiAgent.py
+++ MultiAgent.py
@@ -25,19 +25,20 @@ def mazePrint(maze,goal,start):
         line = line + "■ "
         print(line)
 
-MAX_MOVES = 16
+MAX_MOVES = 30
 NUM_AGENTS = 2
 maze_test = cm.create_maze(4,4,0.75,0.75)
 maze_easy = cm.create_maze(5,5,0.8,0.8)
 maze_medium = cm.create_maze(8,8,0.25,0.25)
 maze_hard = cm.create_maze(10,10,0.40,0.2)
 maze_extreme = cm.create_maze(30,30,0.6,0.75)
-maze = maze_easy
+maze = maze_test
+first_agent = []
 
 #TODO:: THIS IS COMPLETELY ARBITRARY!!
 start = [0,0]
 start2 = [4,0]
-end = [2,2]
+end = [4,4]
 
 class MultiAgent:
     def __init__(self,starting):
@@ -53,15 +54,83 @@ class MultiAgent:
 mazePrint(maze,end,start2)
 #Heuristic/fitness function. Input is individual (can treat like a list)
 #output is score, Note: the comma is important
-def evalOneMax(individual):
-    print(individual)
-    return sum(individual),
+def evalMazeSingle(ind, verbose = False):
+    seen = set()
+    penalty = 0
+    c_c=[0,0]
+    c_c[0] = start[0]
+    c_c[1] = start[1]
+    n_c = [-1,-1]
+    path_length = 0
 
-def evalMaze(inds, verbose = False):
+    for i in range(len(ind)):
+        n_c[0] = c_c[0]
+        n_c[1] = c_c[1]
+        path_length += 1
+        seen.add((c_c[0],c_c[1]))
+        if ind[i] == 1:
+            n_c[0] = c_c[0] - 1
+            if verbose:
+                print("moving up " + str(c_c) + " to " + str(n_c))
+        elif (ind[i] == 2):
+            n_c[1] = c_c[1] + 1
+            if verbose:
+                print("moving right " + str(c_c) + " to " + str(n_c))
+        elif (ind[i] == 3):
+            n_c[0] = c_c[0] + 1
+            if verbose:
+                print("moving down " + str(c_c) + " to " + str(n_c))
+        elif (ind[i] == 4):
+            n_c[1] = c_c[1] - 1
+            if verbose:
+                print("moving left " + str(c_c) + " to " + str(n_c))
+        else:
+            penalty += 0.5
+            if verbose:
+                print("waiting at " + str(c_c))
+        nx = n_c[0]
+        ny = n_c[1]
+
+        if (nx >= len(maze) or ny >= len(maze[0]) or nx < 0 or ny < 0):
+            if verbose:
+                print("OUT OF BOUNDS")
+            penalty+=2
+
+        elif(maze[nx][ny] == False):
+            if verbose:
+                print("INVALID MOVE")
+            penalty+=2
+        else:
+            c_c[0] = n_c[0]
+            c_c[1] = n_c[1]
+            if((n_c[0],n_c[1]) in seen and ind[i] != 0):
+                if verbose: print("seen")
+                penalty+=1
+            if(c_c == end):
+                if verbose:
+                    print("Reached Goal!!")
+                break
+
+    #calculate distance from goal
+    distance = math.sqrt((end[0]-c_c[0])**2 + (end[1]-c_c[1])**2)
+    bonus = 0
+    if distance == 0:
+        bonus = MAX_MOVES-path_length + 100
+    if verbose:
+        print("distance to goal:" + str(distance))
+        print("penalty:" + str(penalty))
+        print("path length:"+ str(path_length))
+
+    return distance + penalty - bonus,
+
+def evalMaze(ind, verbose = False):
+    if(len(first_agent) == 0):
+        return evalMazeSingle(ind)
+    inds = [first_agent,ind]
     penalty = 0
     agents = []
     test = MultiAgent([0,0])
-    test1= MultiAgent([0,4])
+    test1= MultiAgent([4,0])
     agents.append(test)
     agents.append(test1)
     path_len = 0
@@ -93,6 +162,7 @@ def evalMaze(inds, verbose = False):
                     if verbose:
                         print(str(x) + " moving left " + str(a.c_c) + " to " + str(n_c))
                 else:
+                    penalty+=0.5
                     if verbose:
                         print(str(x) + " waiting at " + str(a.c_c))
                 a.move_counter += 1
@@ -128,14 +198,12 @@ def evalMaze(inds, verbose = False):
     #calculate distance from goal
     distance1 = math.sqrt((end[0]-agents[0].c_c[0])**2 + (end[1]-agents[0].c_c[1])**2)
     distance2 = math.sqrt((end[0]-agents[1].c_c[0])**2 + (end[1]-agents[1].c_c[1])**2)
-    distance = distance1 + distance2
+    distance = distance2
     bonus = 0
-    if distance1 == 0:
-        bonus = MAX_MOVES-agents[0].early_stop + 100
     if distance2 == 0:
-        bonus = MAX_MOVES-agents[1].early_stop + 100
+        bonus += MAX_MOVES-agents[1].early_stop
     if distance1 == 0 and distance2 == 0:
-        bonus = MAX_MOVES-agents[0].early_stop +  MAX_MOVES-agents[1].early_stop + 1000
+        bonus+=100
     if verbose:
         print("distance to goal:" + str(distance))
         print("penalty:" + str(penalty))
@@ -147,55 +215,28 @@ creator.create("FitnessMax", base.Fitness, weights=(-1.0,))
 creator.create("Individual", list, fitness=creator.FitnessMax)
 
 toolbox = base.Toolbox()
-toolbox.register("attr_dir", random.randint, 1, 4)
-toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attr_dir, MAX_MOVES)
-toolbox.register("species", tools.initRepeat, list, toolbox.individual, 100)
-
-toolbox.register("mate", tools.cxOnePoint)
-toolbox.register("mutate", tools.mutUniformInt, indpb=0.05,low = 1, up = 4)
-toolbox.register("select", tools.selTournament, tournsize=3)
-toolbox.register("get_best", tools.selBest, k=1)
+toolbox.register("attr_bool", random.randint, 0, 4)
+toolbox.register("individual", tools.initRepeat, creator.Individual,
+    toolbox.attr_bool, MAX_MOVES)
+toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 toolbox.register("evaluate", evalMaze)
+toolbox.register("mutate", tools.mutUniformInt, indpb=0.05,low = 0, up = 4)
+toolbox.register("mate", tools.cxOnePoint)
+toolbox.register("select", tools.selTournament, tournsize=3)
 
-# pop = toolbox.population(n = 1000)
-# hof = tools.HallOfFame(1)
-#
-# pop, log = algorithms.eaSimple(pop, toolbox, cxpb=0.50, mutpb=0.25, ngen=70,
-#                                halloffame=hof, verbose=True)
-#
-# print(hof[0])
-#
-# print(evalMaze(hof[0],verbose = True))
+pop = toolbox.population(n = 500)
+hof1 = tools.HallOfFame(1)
 
-species = [toolbox.species() for _ in range(NUM_AGENTS)]
-representatives = [random.choice(species[i]) for i in range(NUM_AGENTS)]
-best = [random.choice(species[i]) for i in range(NUM_AGENTS)]
-species_index = list(range(NUM_AGENTS))
-g = 1
-ngen = 200
+pop, log = algorithms.eaSimple(pop, toolbox, cxpb=0.50, mutpb=0.25, ngen=50,
+                               halloffame=hof1, verbose=False)
 
-while g < ngen:
-    print(str(g) + ": " + str(len(species[0])))
-        # Initialize a container for the next generation representatives
-    next_repr = [None] * len(species)
-    for (i, s), j in zip(enumerate(species), species_index):
-        # Vary the species individuals
-        s = algorithms.varAnd(s, toolbox, 0.5,0.25)
+first_agent = hof1[0]
+print(hof1[0])
 
-        # Get the representatives excluding the current species
-        r = representatives[:i] + representatives[i+1:]
-        for ind in s:
-            # Evaluate and set the individual fitness
-            ind.fitness.values = toolbox.evaluate([ind] + r)
-        # Select the individuals
-        species[i] = toolbox.select(s, len(s))  # Tournament selection
-        next_repr[i] = toolbox.get_best(s)[0]   # Best selection
+pop2 = toolbox.population(n = 500)
+hof2 = tools.HallOfFame(1)
 
-    representatives = next_repr
-
-    g+=1
-
-print(next_repr)
-print(next_repr[0].fitness.values)
-print(next_repr[1].fitness.values)
-print (evalMaze(best, verbose = True))
+pop2, log = algorithms.eaSimple(pop2, toolbox, cxpb=0.50, mutpb=0.25, ngen=50,
+                               halloffame=hof2, verbose=False)
+print(hof2[0])
+print(evalMaze(hof2[0],verbose = True))
